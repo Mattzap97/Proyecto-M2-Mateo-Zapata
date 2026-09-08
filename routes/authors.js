34bc428b7f0f051ee41022fc8a2018f74b7c778a
Mattzap97 <mattalexxz@gmail.com>
2026-09-07 23:49:32 -0500

    Definiendo CRUD con conexiones Pool hacia la base de datos en PostgreSQL en authors.js, READ funcional

--- routes/authors.js
+++ routes/authors.js
@@ -60,7 +60,28 @@ router.get('/',  async (req, res) => {
 })*/
 
 //GET /api/authors/:id - Obtener un autor por id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+
+    try{
+
+        const result = await pool.query('SELECT * FROM authors WHERE id = $1', [req.params.id]);
+
+        if(result.rows.length === 0) {
+            return res.status(404).json({error: 'Autor no encontrado'});
+        }
+
+        res.json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error('Error al obtener autor', error.message)
+        res.status(500).json({error: 'Error al obtener autor'});
+        
+    }
+    
+})
+
+/*router.get('/:id', (req, res) => {
 
     const author = authors.find(a => a.id === parseInt(req.params.id));
 
@@ -69,7 +90,7 @@ router.get('/:id', (req, res) => {
     }
     res.json(author);
 
-})
+})*/
 
 //POST /api/authors - Crear un nuevo autor
 router.post('/', (req, res) => {
